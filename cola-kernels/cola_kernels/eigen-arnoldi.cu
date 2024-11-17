a24a59402cd5f1a675060dfb37d19e779af937fa
satyachillale <satya.narayana2406@gmail.com>
2024-11-17 14:47:30 -0500

    eigen arnoldi no nan but seg fault kernel

--- cola-kernels/cola_kernels/eigen-arnoldi.cu
+++ cola-kernels/cola_kernels/eigen-arnoldi.cu
@@ -17,7 +17,7 @@ __global__ void norm_new_vector(cuFloatComplex *v, cfloat norm, int n);
 __global__ void update_angle(cuFloatComplex *h_vec, cfloat ang, int i);
 __global__ void update_arr(cuFloatComplex *Q, cuFloatComplex *V, int N, int M, int i);
 __global__ void matrix_vector_mul(float *A, cuFloatComplex *B, cuFloatComplex *C, int N, int M, int i);
-__global__ void update_new_vector(cfloat A, cuFloatComplex *B, cuFloatComplex *C, int N, int M, int i);
+__global__ void update_new_vector(cuFloatComplex *A, cuFloatComplex *B, cuFloatComplex *C, int N, int M, int i);
 __global__ void update_new_vector_clip(cuFloatComplex *A, cuFloatComplex *norm, int N);
 __global__ void angle_cal(cuFloatComplex *Q, cuFloatComplex *V, cuFloatComplex *ang, int N, int M, int i);
 int main(int argc, char *argv[])
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
     double diff_t;
     unsigned int N = atoi(argv[1]);
     int idx = 0;
-    int max_iters = 10;
+    int max_iters = atoi(argv[2]);
     cfloat norm, angle;
     srand(21);
     int random_number = rand();
@@ -57,15 +57,16 @@ int main(int argc, char *argv[])
     {
         for (int j = 0; j < N; j++)
         {
-            int random_int = rand() % 100;
-            A[index(i, j, N)] = (float)random_int;
+            // int random_int = rand() % 100;
+            // A[index(i, j, N)] = (float)random_int;
+            A[index(i, j, N)] = float(i * N + j), 0.0f;
         }
     }
 
     for (int i = 0; i < N; i++)
     {
-        // new_vector[i] = cfloat(float(i), 0.0f);
-        new_vector[i] = ((float)rand() / RAND_MAX);
+        new_vector[i] = cfloat(float(i), 0.0f);
+        // new_vector[i] = ((float)rand() / RAND_MAX);
     }
 
     time(&start_t);
@@ -74,6 +75,8 @@ int main(int argc, char *argv[])
     cudaMemcpy(new_vector_d_r, new_vector_r, N * sizeof(cfloat), cudaMemcpyHostToDevice);
     cudaMemcpy(H_d, H, max_iters * (max_iters + 1) * sizeof(cfloat), cudaMemcpyHostToDevice);
     cudaMemcpy(Q_d, Q, N * (max_iters + 1) * sizeof(cfloat), cudaMemcpyHostToDevice);
+    cudaMemcpy(ang_d, ang, N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(h_vec_d, h_vec, (max_iters + 1) * sizeof(float), cudaMemcpyHostToDevice);
 
     // norm calcuations
     norm_cal<<<(N + 255) / 256, 256>>>(new_vector_d, new_vector_d_r, N);
@@ -84,42 +87,21 @@ int main(int argc, char *argv[])
         norm = norm + new_vector_r[i];
     }
     norm = sqrt(norm);
-    std::cout << "(" << norm.real() << ", " << norm.imag() << "i)" << std::endl;
+    std::cout << "norm(" << norm.real() << ", " << norm.imag() << "i)" << std::endl;
     norm_new_vector<<<(N + 255) / 256, 256>>>(new_vector_d, norm, N);
-    // cudaMemcpy(new_vector, new_vector_d, N * sizeof(cfloat), cudaMemcpyDeviceToHost);
-    //  for (int i = 0; i < N; i++)
-    //  {
-    //      std::cout << "(" << new_vector[i].real() << ", " << new_vector[i].imag() << "i)" << std::endl;
-    //  }
 
     // update Q
-    cudaMemcpy(Q_d, Q, N * (max_iters + 1) * sizeof(cfloat), cudaMemcpyHostToDevice);
     update_arr<<<(N + 255) / 256, 256>>>(Q_d, new_vector_d, N, (max_iters + 1), 0);
-    // cudaMemcpy(Q, Q_d, N * (max_iters + 1) * sizeof(cfloat), cudaMemcpyDeviceToHost);
-    // for (int i = 0; i < N; i++)
-    // {
-    //     std::cout << "(" << Q[index(i, 0, (max_iters + 1))].real() << ", " << Q[index(i, 0, (max_iters + 1))].imag() << "i)" << std::endl;
-    // }
     while (idx < max_iters)
     {
         matrix_vector_mul<<<(N + 255) / 256, 256>>>(A_d, Q_d, new_vector_d, N, (max_iters + 1), idx);
-        // cudaMemcpy(new_vector, new_vector_d, N * sizeof(cfloat), cudaMemcpyDeviceToHost);
-        // for (int i = 0; i < N; i++)
-        // {
-        //     std::cout << "(" << new_vector[i].real() << ", " << new_vector[i].imag() << "i)" << std::endl;
-        // }
-        cudaMemcpy(h_vec_d, h_vec, (max_iters + 1) * sizeof(cuFloatComplex), cudaMemcpyHostToDevice);
+        cudaMemset(h_vec_d, 0, (max_iters + 1) * sizeof(cuFloatComplex));
         for (int j = 0; j < idx + 1; j++)
         {
-            cudaMemcpy(ang_d, ang, N * sizeof(cuFloatComplex), cudaMemcpyHostToDevice);
-            angle_cal<<<(N + 255) / 256, 256>>>(Q_d, new_vector_d, ang_d, N, (max_iters + 1), j);
-            cudaMemcpy(ang, ang_d, N * sizeof(cuFloatComplex), cudaMemcpyDeviceToHost);
-            //     for (int i = 0; i < N; i++)
-            // {
-            //     std::cout << "(" << ang[i].real() << ", " << ang[i].imag() << "i)" << std::endl;
-            // }
+            cudaMemset(ang_d, 0, N * sizeof(cuFloatComplex));
             cudaMemset(new_vector_d_r, 0, (N + 255) / 256 * sizeof(cuFloatComplex));
             memset(new_vector_r, 0, (N + 255) / 256 * sizeof(cfloat));
+            angle_cal<<<(N + 255) / 256, 256>>>(Q_d, new_vector_d, ang_d, N, (max_iters + 1), j);
             sum_reduction<<<(N + 255) / 256, 256>>>(ang_d, new_vector_d_r, N);
             cudaMemcpy(new_vector_r, new_vector_d_r, (N + 255) / 256 * sizeof(cuFloatComplex), cudaMemcpyDeviceToHost);
             angle = 0;
@@ -129,7 +111,7 @@ int main(int argc, char *argv[])
             }
             std::cout << "angle(" << angle.real() << ", " << angle.imag() << "i)" << std::endl;
             update_angle<<<1, 1>>>(h_vec_d, angle, j);
-            update_new_vector<<<(N + 255) / 256, 256>>>(angle, Q_d, new_vector_d, N, (max_iters + 1), j);
+            update_new_vector<<<(N + 255) / 256, 256>>>(h_vec_d, Q_d, new_vector_d, N, (max_iters + 1), j);
         }
         cudaMemset(new_vector_d_r, 0, (N + 255) / 256 * sizeof(cuFloatComplex));
         memset(new_vector_r, 0, (N + 255) / 256 * sizeof(cfloat));
@@ -141,39 +123,22 @@ int main(int argc, char *argv[])
             norm = norm + new_vector_r[i];
         }
         norm = sqrt(norm);
-        std::cout << "(" << norm.real() << ", " << norm.imag() << "i)" << std::endl;
-        if (std::abs(norm.real()) < 0.0000001)
+        std::cout << "norm(" << norm.real() << ", " << norm.imag() << "i)" << std::endl;
+        if (std::abs(norm.real()) < 0.0000001 / 2.0)
         {
-            norm.real(0.0000001);
+            norm.real(0.0000001 / 2.0);
             norm.imag(0.0f);
         }
         norm_new_vector<<<(N + 255) / 256, 256>>>(new_vector_d, norm, N);
-        update_angle<<<1, 1>>>(h_vec_d, norm, idx + 1);
-        update_arr<<<(N + 255) / 256, 256>>>(H_d, h_vec_d, N, max_iters, idx);
-        update_arr<<<(N + 255) / 256, 256>>>(Q_d, new_vector_d, N, max_iters + 1, idx + 1);
+        update_angle<<<1, 1>>>(h_vec_d, norm, (idx + 1));
+        update_arr<<<((max_iters + 1) + 255) / 256, 256>>>(H_d, h_vec_d, (max_iters + 1), max_iters, idx);
+        update_arr<<<(N + 255) / 256, 256>>>(Q_d, new_vector_d, N, (max_iters + 1), (idx + 1));
         idx += 1;
     }
 
     cudaMemcpy(H, H_d, (max_iters + 1) * max_iters * sizeof(cfloat), cudaMemcpyDeviceToHost);
     cudaMemcpy(Q, Q_d, N * (max_iters + 1) * sizeof(cfloat), cudaMemcpyDeviceToHost);
 
-    // for (int i = 0; i < max_iters + 1; i++)
-    // {
-    //     for (int j = 0; j < max_iters; j++)
-    //     {
-    //         std::cout << "(" << H[index(i, j, max_iters)].real() << ", " << H[index(i, j, max_iters)].imag() << "i)";
-    //     }
-    //     printf("\n");
-    // }
-
-    // for (int i = 0; i < N; i++)
-    // {
-    //     for (int j = 0; j < max_iters + 1; j++)
-    //     {
-    //         std::cout << "(" << Q[index(i, j, max_iters+1)].real() << ", " << Q[index(i, j, max_iters+1)].imag() << "i)";
-    //     }
-    //     printf("\n");
-    // }
     time(&end_t);
     diff_t = difftime(end_t, start_t);
     printf("Elapsed wall-clock time: %f seconds\n", diff_t);
@@ -295,16 +260,14 @@ __global__ void matrix_vector_mul(float *A, cuFloatComplex *B, cuFloatComplex *C
     }
 }
 
-__global__ void update_new_vector(cfloat A, cuFloatComplex *B, cuFloatComplex *C, int N, int M, int i)
+__global__ void update_new_vector(cuFloatComplex *A, cuFloatComplex *B, cuFloatComplex *C, int N, int M, int i)
 {
     int row_idx = blockIdx.x * blockDim.x + threadIdx.x;
 
-    // Ensure we only process rows in the valid range
     if (row_idx < N)
     {
-        cuFloatComplex a = make_cuFloatComplex(A.real(), A.imag());
         int idx = index(row_idx, i, M);
-        C[row_idx] = cuCsubf(C[row_idx], cuCmulf(a, B[idx]));
+        C[row_idx] = cuCsubf(C[row_idx], cuCmulf(A[i], B[idx]));
     }
 }
 
@@ -328,3 +291,31 @@ __global__ void angle_cal(cuFloatComplex *Q, cuFloatComplex *V, cuFloatComplex *
         ang[row_idx] = cuCmulf(cuConjf(Q[idx]), V[row_idx]);
     }
 }
+
+// cudaMemset(ang_d, 0, N * sizeof(cuFloatComplex));
+// cudaMemset(new_vector_d_r, 0, (N + 255) / 256 * sizeof(cuFloatComplex));
+// memset(new_vector_r, 0, (N + 255) / 256 * sizeof(cfloat));
+
+// cudaMemcpy(new_vector, new_vector_d, N * sizeof(cfloat), cudaMemcpyDeviceToHost);
+// for (int i = 0; i < N; i++)
+// {
+//     std::cout << "(" << new_vector[i].real() << ", " << new_vector[i].imag() << "i)" << std::endl;
+// }
+
+// for (int i = 0; i < max_iters + 1; i++)
+// {
+//     for (int j = 0; j < max_iters; j++)
+//     {
+//         std::cout << "(" << H[index(i, j, max_iters)].real() << ", " << H[index(i, j, max_iters)].imag() << "i)";
+//     }
+//     printf("\n");
+// }
+
+// for (int i = 0; i < N; i++)
+// {
+//     for (int j = 0; j < max_iters + 1; j++)
+//     {
+//         std::cout << "(" << Q[index(i, j, max_iters+1)].real() << ", " << Q[index(i, j, max_iters+1)].imag() << "i)";
+//     }
+//     printf("\n");
+// }
